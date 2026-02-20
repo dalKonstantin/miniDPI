66d8a4d537ce73448afec02c37a252278a5f4d63
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2026-02-20 20:14:12 +0300

    upd: RingBuffer push() implementation

--- include/core/ring_buffer.hpp
+++ include/core/ring_buffer.hpp
@@ -2,6 +2,9 @@
 #define RING_BUFFER_HPP
 
 #include <atomic>
+#include <cstddef>
+#include <exception>
+#include <optional>
 #include <stdexcept>
 #include <vector>
 
@@ -18,18 +21,45 @@ public:
     buffer_.resize(capacity_);
   }
 
-  bool is_empty() { return size_ == 0; };
-  bool is_full() { return size_ == capacity_; }
+  bool is_empty() const noexcept { return size_ == 0; };
+  bool is_full() const noexcept { return size_ == capacity_; }
+  std::size_t size() const noexcept { return size_; }
 
-  void push(T val);
-  T pop();
+  bool push(T val) noexcept {
+    // buff[tail % capacity] = val
+    // tail = tail + 1
+    // size + 1
+
+    auto current_tail = head_.load(std::memory_order_relaxed);
+    auto next_tail = (current_tail + 1) % capacity_;
+    auto current_head = head_.load(std::memory_order_acquire);
+    if (next_tail == current_head) {
+      return false;
+    }
+
+    buffer_[current_tail] = val;
+
+    ++size_;
+    tail_.store(next_tail, std::memory_order_release);
+    return true;
+  }
+
+  std::optional<T> pop() noexcept {
+    // current_head = head
+    // head = (head + 1) % capacity
+    // size - 1
+    // return buffer[head]
+
+    auto current_tail = tail_.load(std::memory_order::relaxed);
+    auto current_head = head_.load(std::memory_order::acquire);
+  }
 
 private:
-  std::atomic<std::size_t> capacity_;
-  std::atomic<std::size_t> size_;
-  std::atomic<std::size_t> head_;
-  std::atomic<std::size_t> tail_;
-  std::vector<T> buffer_;
+  alignas(64) std::atomic<std::size_t> capacity_;
+  alignas(64) std::atomic<std::size_t> size_;
+  alignas(64) std::atomic<std::size_t> head_;
+  alignas(64) std::atomic<std::size_t> tail_;
+  alignas(64) std::vector<T> buffer_;
 };
 } // namespace core
 #endif // RING_BUFFER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include "core/ring_buffer.hpp"
 #include "log/logger.hpp"
+#include <iostream>
 int main() {
   logger::Logger::get_instance()->set_level_from_env("A_LOG");
 
   LOG_INFO("Starting miniDPI");
 
-  auto rb = core::RingBuffer<int>(20);
+  auto rb = core::RingBuffer<int>(16);
+
+  for (std::size_t i = 0; i < 16; ++i) {
+    if (rb.push(i))
+      LOG_TRACE("Pushed {} to RingBuffer. Size {}", i, rb.size());
+  }
+
+  for (std::size_t i = 0; i < 16; ++i) {
+    auto x = rb.pop();
+    LOG_TRACE("Poped {} from RingBuffer", *x);
+  }
 }
