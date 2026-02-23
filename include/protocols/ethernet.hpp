319732a9c89b1eae20f20ea55de144b0fbe62cc5
dalkonstantin <konser.dal@yandex.com>
2026-02-23 23:40:12 +0300

    upd: ethernet struct optimization

--- include/protocols/ethernet.hpp
+++ include/protocols/ethernet.hpp
@@ -2,10 +2,15 @@
 #define ETHERNET_HPP
 
 #include <cstdint>
+// Save current setting and set new aligment to 1
+#pragma pack(push, 1)
 struct Ethernet {
   uint8_t dst_addr[6];
   uint8_t src_addr[6];
-  uint16_t eth_type;
+  uint16_t eth_type; // Big-Endian
+  // uint32_t checksum; NIC will drop this
 };
+// Restore previous alignment
+#pragma pack(pop)
 
 #endif
---
+++ include/protocols/tcp.hpp
@@ -0,0 +1,11 @@
+#ifndef TCP_HPP
+#define TCP_HPP
+
+#include <cstdint>
+struct Tcp {
+  uint16_t src_port;
+  uint16_t dst_port;
+  uint32_t sn;
+  uint32_t ack_sn;
+};
+#endif
