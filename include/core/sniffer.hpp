2b01915c06c72bd1283ae0348a3c24c82f18fe14
dalkonstantin <konser.dal@yandex.com>
2026-02-26 07:49:16 +0300

    upd: sniffer upd

--- include/core/sniffer.hpp
+++ include/core/sniffer.hpp
@@ -7,7 +7,11 @@
 namespace core {
 class Sniffer {
   Sniffer(std::string_view dev_name, core::RingBuffer<core::Packet> packet_buffer);
+  void loop();
   ~Sniffer();
+
+private:
+  core::RingBuffer<core::Packet> buffer_(std::size_t 4096) {};
 };
 } // namespace core
 #endif
